e4e392e52026bfaef25622704e1ab63898de8526
Shaunan1 <eshaunans@gmail.com>
2025-02-13 10:58:21 +0700

    halaman profil

--- lib/views/profile/profile_screen.dart
+++ lib/views/profile/profile_screen.dart
@@ -1,16 +1,19 @@
-// ignore_for_file: avoid_print, use_build_context_synchronously
-
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
 import 'package:pecut/controllers/sso_controller.dart';
 import 'package:pecut/views/auth/login_screen.dart';
-import 'package:pecut/widgets/profile_list_widget.dart';
 import 'package:provider/provider.dart';
 
-const double iconSize = 32;
-
-class ProfileScreen extends StatelessWidget {
+class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
+
+  @override
+  _ProfileScreenState createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  bool _showEmail = false;
+  bool _showNik = false;
 
   @override
   Widget build(BuildContext context) {
@@ -23,7 +26,7 @@ class ProfileScreen extends StatelessWidget {
         );
       }
 
-      if (sso.isAuth == false) {
+      if (!sso.isAuth) {
         return const LoginScreen();
       }
 
@@ -33,99 +36,81 @@ class ProfileScreen extends StatelessWidget {
         ),
         body: SingleChildScrollView(
           child: Padding(
-            padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 15),
+            padding: const EdgeInsets.all(15),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                ProfileListWidget(
-                  value: sso.user.name!,
-                  label: 'Nama',
-                  icon: Icons.person,
-                  onTap: () {
-                    print('Edit: name');
-                  },
-                ),
-                ProfileListWidget(
-                  value: sso.user.email!,
-                  label: 'Email',
-                  icon: Icons.email,
-                  onTap: () {
-                    print('Edit: email');
-                  },
-                ),
-                ProfileListWidget(
-                  value: sso.user.phone!,
-                  label: 'No. HP (WhatsApp)',
-                  icon: Icons.phone,
-                  onTap: () {
-                    print('Edit: phone');
-                  },
-                ),
-                ProfileListWidget(
-                  value: sso.user.nik!,
-                  label: 'NIK (Nomor KTP)',
-                  icon: Icons.badge,
-                  onTap: () {
-                    print('Edit: nik');
-                  },
-                ),
-                const SizedBox(height: 60),
-                const Text('Preferensi',
-                    style: TextStyle(fontWeight: FontWeight.bold)),
-                const SizedBox(height: 10),
-                ProfileListWidget(
-                  value: 'Keluar dari aplikasi',
-                  label: 'Keluar',
-                  icon: Icons.logout,
-                  onTap: () {
-                    showModalBottomSheet(
-                      context: context,
-                      builder: (context) {
-                        return SizedBox(
-                          height: MediaQuery.sizeOf(context).height / 2.5,
-                          child: Center(
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Lottie.asset(
-                                  'assets/images/lottie/question.json',
-                                  height: 100,
-                                ),
-                                const SizedBox(height: 40),
-                                const Text(
-                                  'Keluar dari aplikasi ?',
-                                  style: TextStyle(fontSize: 18),
-                                ),
-                                const SizedBox(height: 10),
-                                TextButton.icon(
-                                  icon: const Icon(Icons.check,
-                                      color: Colors.white),
-                                  label: const Text(
-                                    'Ya',
-                                    style: TextStyle(color: Colors.white),
-                                  ),
-                                  style: const ButtonStyle(
-                                    padding: WidgetStatePropertyAll(
-                                      EdgeInsets.symmetric(horizontal: 60),
-                                    ),
-                                    backgroundColor:
-                                        WidgetStatePropertyAll(Colors.black),
-                                  ),
-                                  onPressed: () {
-                                    sso.logout().then((res) {
-                                      Navigator.pop(context);
-                                      print(
-                                        'logoutResponse: ${res.toString()}',
-                                      );
-                                    });
-                                  },
-                                ),
-                              ],
+                // Kartu Identitas
+                Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  elevation: 3,
+                  child: Padding(
+                    padding: const EdgeInsets.all(16),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Center(
+                          child: CircleAvatar(
+                            radius: 40,
+                            backgroundImage: const NetworkImage(
+                                'https://randomuser.me/api/portraits/men/64.jpg'),
+                          ),
+                        ),
+                        const SizedBox(height: 16),
+                        Center(
+                          child: Text(
+                            sso.user.name!,
+                            style: const TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
                             ),
                           ),
-                        );
-                      },
-                    );
+                        ),
+                        const Divider(height: 30),
+                        _buildProfileRow(
+                          label: 'Email',
+                          value:
+                              _showEmail ? sso.user.email! : '******@****.***',
+                          onTap: () {
+                            setState(() {
+                              _showEmail = !_showEmail;
+                            });
+                          },
+                          isHidden: !_showEmail,
+                        ),
+                        const SizedBox(height: 10),
+                        _buildProfileRow(
+                          label: 'NIK (Nomor KTP)',
+                          value:
+                              _showNik ? sso.user.nik! : '**** **** **** ****',
+                          onTap: () {
+                            setState(() {
+                              _showNik = !_showNik;
+                            });
+                          },
+                          isHidden: !_showNik,
+                        ),
+                        const SizedBox(height: 10),
+                        _buildProfileRow(
+                          label: 'No. HP (WhatsApp)',
+                          value: sso.user.phone!,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 30),
+                const Text('Preferensi',
+                    style:
+                        TextStyle(fontWeight: FontWeight.bold, fontSize: 16)),
+                const SizedBox(height: 10),
+                ListTile(
+                  leading: const Icon(Icons.logout, color: Colors.red),
+                  title: const Text('Keluar dari aplikasi'),
+                  onTap: () {
+                    _showLogoutConfirmation(context, sso);
                   },
                 ),
               ],
@@ -134,5 +119,130 @@ class ProfileScreen extends StatelessWidget {
         ),
       );
     });
+  }
+
+  Widget _buildProfileRow({
+    required String label,
+    required String value,
+    VoidCallback? onTap,
+    bool isHidden = false,
+  }) {
+    String formattedValue = value;
+    if (label == 'No. HP (WhatsApp)') {
+      formattedValue = _formatPhone(value, isHidden);
+    } else if (label == 'Email') {
+      formattedValue = _formatEmail(value, isHidden);
+    } else if (label == 'NIK (Nomor KTP)') {
+      formattedValue = _formatNik(value, isHidden);
+    }
+
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                label,
+                style: const TextStyle(
+                  fontSize: 14,
+                  color: Colors.black54,
+                ),
+              ),
+              const SizedBox(height: 4),
+              Text(
+                formattedValue,
+                style: const TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+        ),
+        if (onTap != null)
+          IconButton(
+            icon: Icon(
+              isHidden ? Icons.visibility_off : Icons.visibility,
+              color: Colors.grey,
+            ),
+            onPressed: onTap,
+          ),
+      ],
+    );
+  }
+
+  String _formatPhone(String phone, bool isHidden) {
+    if (isHidden)
+      return '${phone.substring(0, 3)}****${phone.substring(phone.length - 3)}';
+    return phone;
+  }
+
+  String _formatEmail(String email, bool isHidden) {
+    if (isHidden) {
+      final atIndex = email.indexOf('@');
+      if (atIndex > 2) {
+        final prefix = email.substring(0, 1);
+        final suffix = email.substring(atIndex - 1, atIndex);
+        return '$prefix****$suffix${email.substring(atIndex)}';
+      }
+    }
+    return email;
+  }
+
+  String _formatNik(String nik, bool isHidden) {
+    if (isHidden && nik.length > 6) {
+      return '${nik.substring(0, 3)}****${nik.substring(nik.length - 3)}';
+    }
+    return nik;
+  }
+
+  void _showLogoutConfirmation(BuildContext context, SsoController sso) {
+    showModalBottomSheet(
+      context: context,
+      builder: (context) {
+        return SizedBox(
+          height: MediaQuery.of(context).size.height / 3,
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Lottie.asset(
+                  'assets/images/lottie/question.json',
+                  height: 100,
+                ),
+                const SizedBox(height: 20),
+                const Text(
+                  'Keluar dari aplikasi?',
+                  style: TextStyle(fontSize: 18),
+                ),
+                const SizedBox(height: 20),
+                TextButton.icon(
+                  icon: const Icon(Icons.check, color: Colors.white),
+                  label: const Text(
+                    'Ya',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.black,
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 40,
+                      vertical: 10,
+                    ),
+                  ),
+                  onPressed: () {
+                    sso.logout().then((res) {
+                      Navigator.pop(context);
+                      print('logoutResponse: ${res.toString()}');
+                    });
+                  },
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
   }
 }
