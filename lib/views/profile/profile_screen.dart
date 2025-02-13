6169500b1a8d485a85121514e613e4fbb53af2f4
Shaunan1 <eshaunans@gmail.com>
2025-02-13 11:11:43 +0700

    testing uis

--- lib/views/profile/profile_screen.dart
+++ lib/views/profile/profile_screen.dart
@@ -82,7 +82,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
                         ),
                         const SizedBox(height: 10),
                         _buildProfileRow(
-                          label: 'NIK (Nomor KTP?)',
+                          label: 'NIK (Nomor KTPuis)',
                           value:
                               _showNik ? sso.user.nik! : '**** **** **** ****',
                           onTap: () {
