c05a7a058a28170fe9038f1ceab371cd0ba84669
Syuanita <syafirayuanita14@gmail.com>
2025-02-14 09:39:24 +0700

    Merge branch 'main' of https://github.com/shaunan1/KerjaPraktekAndroid2

--- lib/views/auth/login_screen.dart
+++ lib/views/auth/login_screen.dart
@@ -1,11 +1,7 @@
-// ignore_for_file: avoid_print
-
 import 'package:flutter/material.dart';
 import 'package:pecut/controllers/sso_controller.dart';
 import 'package:pecut/widgets/text_form_field_widget.dart';
 import 'package:provider/provider.dart';
-
-const int alpha = 25;
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -30,6 +26,7 @@ class _LoginScreenState extends State<LoginScreen> {
     ScaffoldMessenger.of(context).showSnackBar(
       SnackBar(
         content: Text(body),
+        backgroundColor: Colors.redAccent,
       ),
     );
   }
@@ -43,142 +40,133 @@ class _LoginScreenState extends State<LoginScreen> {
             FocusManager.instance.primaryFocus?.unfocus();
           },
           child: Scaffold(
-            appBar: AppBar(
-              title: const Text('Login'),
-            ),
-            body: SingleChildScrollView(
-              child: Padding(
-                padding: const EdgeInsets.all(30.0),
-                child: Column(
-                  children: [
-                    Text(
-                      'PECUT',
-                      style: TextStyle(
-                        fontWeight: FontWeight.w900,
-                        fontSize: 36,
-                        color: Theme.of(context).colorScheme.primary,
-                      ),
+            body: Stack(
+              children: [
+                Container(
+                  decoration: const BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: [
+                        Color.fromARGB(255, 255, 255, 255),
+                        Color.fromARGB(255, 200, 223, 255)
+                      ],
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
                     ),
-                    const Text(
-                      'Portal Efisien Cepat Mudah Terpadu',
-                      style: TextStyle(
-                        fontSize: 16,
-                        color: Colors.black54,
-                      ),
-                    ),
-                    const Text(
-                      'Pemerintah Kota Kediri',
-                      style: TextStyle(
-                        fontSize: 16,
-                        color: Colors.black54,
-                      ),
-                    ),
-                    const Image(
-                      image: AssetImage('assets/images/logo-app.png'),
-                      height: 250,
-                    ),
-                    Form(
-                      key: formKey,
+                  ),
+                ),
+                Center(
+                  child: SingleChildScrollView(
+                    child: Padding(
+                      padding: const EdgeInsets.all(20.0),
                       child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
                         children: [
-                          TextFormFieldWidget(
-                            attributeCtrl: emailCtrl,
-                            labelText: 'Email',
-                            iconData: Icons.person,
+                          const Image(
+                            image: AssetImage('assets/images/logo-app.png'),
+                            height: 150,
                           ),
-                          const SizedBox(height: 15),
-                          TextFormField(
-                            controller: passwordCtrl,
-                            obscureText: _isSecure,
-                            decoration: InputDecoration(
-                              filled: true,
-                              fillColor: Colors.white,
-                              labelText: 'Password',
-                              prefixIcon: Icon(
-                                Icons.lock,
-                                color: Theme.of(context).colorScheme.primary,
-                              ),
-                              suffixIcon: InkWell(
-                                child: Icon(
-                                  _isSecure
-                                      ? Icons.visibility
-                                      : Icons.visibility_off,
-                                  color: Colors.grey[400],
-                                ),
-                                onTap: () => toggleSecure(),
-                              ),
-                              enabledBorder: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(10),
-                                borderSide: const BorderSide(
-                                  color: Colors.white,
-                                ),
-                              ),
-                              focusedBorder: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(10),
-                                borderSide: BorderSide(
-                                  color: Colors.grey[300]!,
-                                ),
-                              ),
-                              errorBorder: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(10),
-                                borderSide: BorderSide(
-                                  color: Theme.of(context).colorScheme.error,
-                                ),
-                              ),
-                              focusedErrorBorder: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(10),
-                                borderSide: BorderSide(
-                                  color: Theme.of(context).colorScheme.error,
-                                ),
-                              ),
+                          const SizedBox(height: 20),
+                          Text(
+                            'Welcome to PECUT',
+                            style: TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black,
                             ),
-                            validator: (value) {
-                              if (value == null || value.isEmpty) {
-                                return 'Password field is required';
-                              }
-                              return null;
-                            },
+                          ),
+                          const SizedBox(height: 5),
+                          const Text('Portal Efisien Cepat Mudah Terpadu',
+                              style:
+                                  TextStyle(color: Colors.black, fontSize: 16)),
+                          const SizedBox(height: 30),
+                          Form(
+                            key: formKey,
+                            child: Column(
+                              children: [
+                                TextFormFieldWidget(
+                                  attributeCtrl: emailCtrl,
+                                  labelText: 'Email',
+                                  iconData: Icons.email,
+                                ),
+                                const SizedBox(height: 15),
+                                TextFormField(
+                                  controller: passwordCtrl,
+                                  obscureText: _isSecure,
+                                  decoration: InputDecoration(
+                                    filled: true,
+                                    fillColor: Colors.white,
+                                    labelText: 'Password',
+                                    prefixIcon: Icon(Icons.lock),
+                                    suffixIcon: InkWell(
+                                      child: Icon(
+                                        _isSecure
+                                            ? Icons.visibility
+                                            : Icons.visibility_off,
+                                      ),
+                                      onTap: () => toggleSecure(),
+                                    ),
+                                    border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(10),
+                                    ),
+                                  ),
+                                  validator: (value) {
+                                    if (value == null || value.isEmpty) {
+                                      return 'Password is required';
+                                    }
+                                    return null;
+                                  },
+                                ),
+                                const SizedBox(height: 20),
+                                SizedBox(
+                                  width: double.infinity,
+                                  child: ElevatedButton(
+                                    style: ElevatedButton.styleFrom(
+                                      padding: const EdgeInsets.symmetric(
+                                          vertical: 15),
+                                      backgroundColor: Colors.white,
+                                      foregroundColor: Colors.black,
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(10),
+                                      ),
+                                      elevation: 5,
+                                    ),
+                                    onPressed: () {
+                                      if (formKey.currentState!.validate()) {
+                                        sso
+                                            .login(emailCtrl.text,
+                                                passwordCtrl.text)
+                                            .then((res) {
+                                          handleSnackbar(
+                                              res['message'].toString());
+                                        });
+                                      }
+                                    },
+                                    child: Text(
+                                      sso.isLoadingLogin
+                                          ? 'Processing...'
+                                          : 'Login with SSO',
+                                      style: const TextStyle(
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
                           ),
                           const SizedBox(height: 15),
-                          SizedBox(
-                            width: double.infinity,
-                            child: TextButton(
-                              style: ButtonStyle(
-                                padding: const WidgetStatePropertyAll(
-                                  EdgeInsets.symmetric(vertical: 15),
-                                ),
-                                shape: WidgetStatePropertyAll(
-                                    RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(10),
-                                )),
-                                backgroundColor: WidgetStatePropertyAll(
-                                  sso.isLoadingLogin
-                                      ? Colors.black38
-                                      : Colors.black,
-                                ),
-                                foregroundColor:
-                                    const WidgetStatePropertyAll(Colors.white),
-                              ),
-                              onPressed: () {
-                                if (formKey.currentState!.validate()) {
-                                  sso
-                                      .login(emailCtrl.text, passwordCtrl.text)
-                                      .then((res) {
-                                    handleSnackbar(res['message'].toString());
-                                  });
-                                }
-                              },
-                              child: Text(sso.isLoadingLogin
-                                  ? 'Processing...'
-                                  : 'Login with SSO'),
+                          TextButton(
+                            onPressed: () {},
+                            child: const Text(
+                              'Forgot Password?',
+                              style: TextStyle(color: Colors.black),
                             ),
                           ),
                         ],
                       ),
                     ),
-                  ],
+                  ),
                 ),
-              ),
+              ],
             ),
           ),
         );
